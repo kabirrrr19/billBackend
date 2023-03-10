4741f0661ca80a12a10f3c0d9f6e5ef29617daea
kabirrrr19 <kabirrrr0402@gmail.com>
2023-03-10 18:41:44 +0530

    build version-1

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ const billModel = require('./models/bill.model');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
